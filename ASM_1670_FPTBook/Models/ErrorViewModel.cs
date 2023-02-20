839c3470f3512f495e8225187ea1002a742f4827
bao0512 <baohcgcc200363@fpt.edu.vn>
2023-02-20 20:45:22 +0700

    Add project files.

---
+++ ASM_1670_FPTBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASM_1670_FPTBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

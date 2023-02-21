7e004d1db0cd9466c1e9f9a30571d51de9e099c3
Nguyễn Công Thể <105303251+harynguyen@users.noreply.github.com>
2023-02-21 14:52:29 +0700

    model

--- ASM_1670_FPTBook/Models/Feedback.cs
+++ ASM_1670_FPTBook/Models/Feedback.cs
@@ -1,6 +1,13 @@
-﻿namespace ASM_1670_FPTBook.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASM_1670_FPTBook.Models
 {
     public class Feedback
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Feedback_Id { get; set; }
+        public string Feedback_Title { get; set; }
+
     }
 }
--- ASM_1670_FPTBook/Program.cs
+++ ASM_1670_FPTBook/Program.cs
@@ -5,9 +5,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+var connectionString = builder.Configuration.GetConnectionString("DefaulConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaulConnection")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)

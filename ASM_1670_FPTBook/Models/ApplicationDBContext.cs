4b2ff6774c665e7cd44eb18c1e7c75cc95c7a50e
myndgcc20002 <100873277+myndgcc20002@users.noreply.github.com>
2023-02-21 14:07:57 +0700

    Add ApplicationDBContext class

---
+++ ASM_1670_FPTBook/Models/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASM_1670_FPTBook.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> books { get; set; }
+        public DbSet<Genre> genres { get; set; }
+        public DbSet<Publisher> publishers { get; set; }
+    }
+}

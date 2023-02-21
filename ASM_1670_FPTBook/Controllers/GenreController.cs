64dd1493fc738e269f67434301eb53f8fa5ce035
myndgcc20002 <100873277+myndgcc20002@users.noreply.github.com>
2023-02-21 17:20:13 +0700

    Genre

--- ASM_1670_FPTBook/Controllers/GenreController.cs
+++ ASM_1670_FPTBook/Controllers/GenreController.cs
@@ -1,12 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASM_1670_FPTBook.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASM_1670_FPTBook.Controllers
 {
     public class GenreController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        public GenreController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        // index
         public IActionResult Index()
         {
+            IEnumerable<Genre> genres = _db.genres.ToList();
+            return View(genres);
+        }
+
+        // create
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        public IActionResult Create(Genre genre)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.genres.Add(genre);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+
+        // edit
+        public IActionResult Edit(int id)
+        {
+            Genre genre= _db.genres.Find(id);
+            if(genre==null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+        [HttpPost]
+        public IActionResult Edit(Genre genre, int id) 
+        {
+            if(ModelState.IsValid)
+            {
+                genre.Gen_Id = id;
+                _db.genres.Add(genre);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+
+        // delete
+        public IActionResult Delete(int id)
+        {
+            Genre genre = _db.genres.Find(id);
+            _db.genres.Remove(genre);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
